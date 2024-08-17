843524cfce0b5153b5dc04f32a5d500510d2f5d3
Xavier936 <xavierlema1@gmail.com>
2024-08-16 23:19:25 -0500

    Actualiacion para implementar DataTable

--- usuarios/index.php
+++ usuarios/index.php
@@ -4,18 +4,18 @@ include('../layout/sesion.php');
 include('../layout/parte1.php');
 include('../app/controllers/usuarios/listado_usuarios.php');
 
-if(isset($_SESSION['mensaje'])){
+if (isset($_SESSION['mensaje'])) {
   $respuesta = $_SESSION['mensaje'];
 
 ?>
-<script>
-  Swal.fire({
-          icon: "success",
-          title: '<?php echo $respuesta; ?>',
-          showConfirmButton: false,
-          timer: 2500
-        });
-</script>
+  <script>
+    Swal.fire({
+      icon: "success",
+      title: '<?php echo $respuesta; ?>',
+      showConfirmButton: false,
+      timer: 2500
+    });
+  </script>
 <?php
   unset($_SESSION['mensaje']);
 }
@@ -23,65 +23,99 @@ if(isset($_SESSION['mensaje'])){
 ?>
 <!-- Content Header (Page header) -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-12">
-            <h1 class="m-0">Listado de usuarios</h1>
-          
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-12">
+          <h1 class="m-0">Listado de usuarios</h1>
 
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-8">
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Usuarios Registrados</h3>
-                <div class="card-tools">
-                  <buutton class="btn btn-tool"><i class="fas fa-minus"></i></buutton>
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <div class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card card-outline card-primary">
+                <div class="card-header">
+                  <h3 class="card-title">Usuarios Registrados</h3>
+                  <div class="card-tools">
+                    <buutton class="btn btn-tool"><i class="fas fa-minus"></i></buutton>
+                  </div>
                 </div>
-              </div>
-              <div class="card-body" style="display: block;">
-                <table class="table table-bordered table-hover">
+                <div class="card">
+              
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
                   <tr>
-                    <th>Nro</th>
-                    <th>Nombres</th>
-                    <th>Email</th>
+                      <th>Nro</th>
+                      <th>Nombres</th>
+                      <th>Email</th>
                   </tr>
+                  </thead>
                   <tbody>
-                    <?php
-                    $contador = 0;  
-                    foreach($data_user as $userdata){
-                      echo "hola a todos";?>
-                      <tr>
-                        <td><?php  echo $contador = $contador +1 ;?></td>
-                        <td><?php  echo $userdata['nombres']?></td>
-                        <td><?php  echo $userdata['email']?></td>
-                      </tr>
-                      <?php  
-                      } ?>
-                  </tbody>
+                      <?php
+                      $contador = 0;
+                      foreach ($data_user as $userdata) { ?>
+                        <tr>
+                          <td><?php echo $contador = $contador + 1; ?></td>
+                          <td><?php echo $userdata['nombres'] ?></td>
+                          <td><?php echo $userdata['email'] ?></td>
+                        </tr>
+                      <?php } ?>
+                    </tbody>
+                  
                 </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
               </div>
             </div>
           </div>
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
+          <!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content -->
     </div>
-    <!-- /.content -->
+    <!-- /.content-wrapper -->
   </div>
-  <!-- /.content-wrapper -->
-    </div>
 </div>
 
+<?php include('../layout/parte2.php'); ?>
 
+<script>
+  $(function() {
+    $("#example1").DataTable({
+      "pageLength": 5,
+      "language":{
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Usuarios",
+        "infoEmpty": "Mostrando 0 to 0 of 0 Usuarios",
+        "infoFiltered": "(Filtrado de _MAX_ total Usuarios)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Usuarios",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscador:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+          "first": "Primero",
+          "last": "Ultimo",
+          "next": "Siguiente",
+          "previous": "Anterior"
+        }
+      },
+      "responsive": true,
+      "lengthChange": true,
+      "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
 
-    <?php
-    include('../layout/parte2.php');
-    ?>
+  });
+</script>
